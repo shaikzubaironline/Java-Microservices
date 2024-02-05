b22adf4ea5b793c5a0fa716ccc1366ae4adf2c46
shaikzubaironline <158131139+shaikzubaironline@users.noreply.github.com>
2024-02-04 19:09:04 -0500

    Final push

---
+++ ConfigServer/src/test/java/com/ShaikZubair/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShaikZubair.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
